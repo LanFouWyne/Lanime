d0e6a450fcc7aa86ae47552cbdde1e8fa8f94098
unknown <pieeesusuuu08@gmail.com>
2025-03-08 02:22:44 +0800

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-// Hapus import node karena kita tidak akan menggunakannya
-// import node from "@astrojs/node";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lanfouwyne.github.io',
-  base: '/Lanime.github.io',
-  
   env: {
     schema: {
       API_URL: envField.string({
@@ -19,13 +15,9 @@ export default defineConfig({
     },
   },
 
-  // Ubah output menjadi "static" untuk GitHub Pages
-  output: "static",
+  output: "server",
 
-  // Hapus adapter node karena tidak diperlukan untuk static site
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
+  adapter: vercel(),
 
   integrations: [tailwind()],
 
