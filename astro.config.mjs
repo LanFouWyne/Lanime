5d678438ce04bc665ab3f16a8642a430d1c351b7
unknown <pieeesusuuu08@gmail.com>
2025-03-08 20:53:24 +0800

    added fix to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({|?
+export default defineConfig({
   env: {
     schema: {
       API_URL: envField.string({
